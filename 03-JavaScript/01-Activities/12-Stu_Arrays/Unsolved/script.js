f8864ec001d0366dd15019dcb41ff418387b3290
MSmart <matt.smart14@gmail.com>
2023-10-27 08:14:45 -0500

    completed class exercises 10-26-23

--- 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,18 @@
 // WRITE YOUR CODE HERE
+// var names = ["Billy","James","Jimmy","Darcy"];
+
+// console.log("Welcome to the class " + names[0]);
+
+
+var names = ["Billy","James","Jimmy","Darcy"];
+console.log(names.length);
+console.log("Welcome to the class " + names[0]);
+console.log("Welcome to the class " + names[1]);
+console.log("Welcome to the class " + names[2]);
+console.log("Welcome to the class " + names[3]);
+
+names[0] = "Bob";
+
+if(names[0] === "Bob") {
+    console.log(names[0] + " is in class!");
+}
--- 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
+++ 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,6 @@
 //WRITE YOUR CODE BELOW
+var students=["Billy","James","Jimmy","Darcy"];
+
+for (var i=0; i < students.length; i++){
+    console.log("Hello, " + students[i]);
+}
